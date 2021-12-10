93cf490a0a454f83767b28f64b60e49a8fccb40a
caldwella2 <caldwella2@allegheny.edu>
2021-12-09 19:15:15 -0500

    changed route to main

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def home():
+@app.route('/main/', methods=['POST', 'GET'])
+def main():
     return render_template('main.html')
 if __name__ == '__main__':
     app.run()
