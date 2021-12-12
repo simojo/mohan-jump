98fe0d0173958735258c30c2aefe0e33327e3513
caldwella2 <caldwella2@allegheny.edu>
2021-12-12 13:50:08 -0500

    changed my app.py and changed link in main.html/ also added a static folder

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
