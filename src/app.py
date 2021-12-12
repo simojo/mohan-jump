d35f4b57b998999959f4a40cda568efe850253c7
Simon Jones <github@simonjjones.com>
2021-12-12 14:11:11 -0500

    asset requests fix?

--- src/app.py
+++ src/app.py
@@ -6,5 +6,19 @@ app = Flask(__name__,
 @app.route('/', methods=['POST', 'GET'])
 def main():
     return render_template('main.html')
+
+# handling asset requests
+@app.route('/css/<path:path>', methods=['GET'])
+def send_css(path):
+    return send_from_directory('css', path)
+
+@app.route('/js/<path:path>', methods=['GET'])
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/imgs/<path:path>', methods=['GET'])
+def send_imgs(path):
+    return send_from_directory('imgs', path)
+
 if __name__ == '__main__':
     app.run()
